6c2d3eec7371c770d84c0ae550df8ef38809b872
Ilham Abdul Ghoni <ilhamabdul66@gmail.com>
2024-05-19 16:20:02 +0700

    konflik

--- routes/cms.php
+++ routes/cms.php
@@ -7,11 +7,7 @@ use App\Http\Controllers\TagController;
 use App\Http\Controllers\DiseaseCategoryController;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\TopicController;
-<<<<<<< HEAD
-use App\Models\Product;
-=======
 use App\Http\Controllers\ProductController;
->>>>>>> 6888dd5a03a0b14585c0d8428a5298532ca0561b
 
 /*
 |--------------------------------------------------------------------------
@@ -47,7 +43,6 @@ Route::group(['prefix' => 'tag','as' => 'tag.'],function(){
     Route::delete('/{topicId}/delete',[TagController::class,'delete'])->name('delete');
 });
 
-<<<<<<< HEAD
 Route::group(['prefix' => 'product-category','as' => 'product-category.'],function(){
     Route::get('/',[ProductCategoryController::class,'getList'])->name('getList');
     Route::get('/{productCategoryId}',[ProductCategoryController::class,'getDetail'])->name('getDetail');
@@ -55,12 +50,12 @@ Route::group(['prefix' => 'product-category','as' => 'product-category.'],functi
     Route::post('/{productCategoryId}/update',[ProductCategoryController::class,'update'])->name('update');
     Route::delete('/{productCategoryId}/delete',[ProductCategoryController::class,'delete'])->name('delete');
     Route::post('/{productCategoryId}/slug',[ProductCategoryController::class,'enableDisable'])->name('enableDisable');
-=======
+});
+
 Route::group(['prefix' => 'product','as' => 'product.'],function(){
     Route::get('/',[ProductController::class,'getList'])->name('getList');
     Route::get('/{productId}',[ProductController::class,'getDetail'])->name('getDetail');
     Route::post('/create',[ProductController::class,'create'])->name('create');
     Route::post('/{productId}/update',[ProductController::class,'update'])->name('update');
     Route::delete('/{productId}/delete',[ProductController::class,'delete'])->name('delete');
->>>>>>> 6888dd5a03a0b14585c0d8428a5298532ca0561b
 });
